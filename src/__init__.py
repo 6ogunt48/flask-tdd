7d8a3e5097a11bce59875446f6bdbaac37c6a074
Tobiloba Ogundiyan <114788338+6ogunt48@users.noreply.github.com>
2023-11-05 21:01:12 +0000

    created app settings and app local dev settings

---
+++ src/__init__.py
@@ -0,0 +1,24 @@
+import os
+import sys
+from flask import Flask, jsonify
+from flask_restx import Resource, Api
+
+# instantiate the app
+app = Flask(__name__)
+api = Api(app)
+
+# set config
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+print(app.config, file=sys.stderr)
+
+
+class Ping(Resource):
+    def get(self):
+        return {
+            'status': 'success',
+            'message': 'pong!'
+        }
+
+
+api.add_resource(Ping, '/ping')
